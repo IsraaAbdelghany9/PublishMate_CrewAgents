be512d42b47a02928bf5af7281da56eabc671072
Your Name <israaabdelghany9@gmail.com>
2025-06-07 05:42:25 +0300

    3 agents Done

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 
 from agents.Trending_Topics_Agent import create_trending_topics_agent_task
 from agents.Recent_Papers_Retrieval_Agent import create_recent_papers_agent_task
+from agents.Research_Gap_and_Suggestion_Agent import create_research_gap_agent_task
+
 from Config.shared import *
 
 # Get user input at runtime
@@ -12,16 +14,20 @@ trending_agent, trending_task = create_trending_topics_agent_task(user_input)
 
 recent_papers_agent, recent_papers_task = create_recent_papers_agent_task()
 
+research_gap_agent, research_gap_task = create_research_gap_agent_task()
+
 # Create the crew with just this task
 crew = Crew(
     agents=[trending_agent,
             recent_papers_agent,
+            research_gap_agent,
             ],
 
     tasks=[trending_task,
            recent_papers_task,
+           research_gap_task,
            ],
-           
+
     verbose=True,
 )
 
