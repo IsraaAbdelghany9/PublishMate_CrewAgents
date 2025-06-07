ba46a626a644b498c3a8427632547fe34e009f26
Your Name <israaabdelghany9@gmail.com>
2025-06-07 06:58:36 +0300

    crew2 agent1 done

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 from agents.Trending_Topics_Agent import create_trending_topics_agent_task
 from agents.Recent_Papers_Retrieval_Agent import create_recent_papers_agent_task
 from agents.Research_Gap_and_Suggestion_Agent import create_research_gap_agent_task
-
+from agents.Search_about_chosen_gab_Agent import create_research_starting_points_agent_task
 from Config.shared import *
 
 # Get user input at runtime
@@ -17,7 +17,7 @@ recent_papers_agent, recent_papers_task = create_recent_papers_agent_task()
 research_gap_agent, research_gap_task = create_research_gap_agent_task()
 
 # Create the crew with just this task
-crew = Crew(
+crew1 = Crew(
     agents=[trending_agent,
             recent_papers_agent,
             research_gap_agent,
@@ -34,6 +34,23 @@ crew = Crew(
 # Run the crew
 # crew.kickoff()
 
-results = crew.kickoff
+results = crew1.kickoff
 
 ## Read the output from the files
+
+
+chosen_topic = input("Which topic did you get interested in more? ")
+chosen_gap = input("Which gap do you like to start looking for ^-^? ")
+
+
+research_starting_points_agent, research_starting_points_task = create_research_starting_points_agent_task(chosen_topic= chosen_topic, chosen_gap= chosen_gap)
+
+crew2 = Crew(
+    agents=[research_starting_points_agent,
+            ],
+
+    tasks=[research_starting_points_task,
+           ],
+
+    verbose=True,
+)
