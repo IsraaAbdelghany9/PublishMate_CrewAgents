f494a87739083b76950b7174c7b01e6a6e19fc76
Israa Abdelghany <116383287+IsraaAbdelghany9@users.noreply.github.com>
2025-06-08 09:13:44 +0300

    Update streamlit_app_2.py

--- app/streamlit_app_2.py
+++ app/streamlit_app_2.py
@@ -1,6 +1,9 @@
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 import os
 import json
-import sys
 import time
 import streamlit as st
 import requests
