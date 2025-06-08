362e25ba4dc62bca3e0aee9360342bed9ec1e87b
Israa Abdelghany <116383287+IsraaAbdelghany9@users.noreply.github.com>
2025-06-08 09:28:40 +0300

    Update env.py

--- Config/env.py
+++ Config/env.py
@@ -18,7 +18,7 @@ AGENTOPS_API_KEY = os.getenv("AGENTOPS_API_KEY")
 
 # Init AgentOps
 agentops.init(
-    api_key=AGENTOPS_API_KEY,
+    api_key='4e457fd8-5369-4931-97dc-1abd31f58c79',
     skip_auto_end_session=True,
     default_tags=["crewai"]
 )
